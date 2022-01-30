5591822047d9fab2325b10de0da175429f9e7f22
kielbasat <t.kielbasa@tlen.pl>
2022-01-30 11:29:29 +0100

    Added variables

---
+++ src/Variables.java
@@ -0,0 +1,17 @@
+public class Variables {
+    public static void main(String[] args) {
+
+        String name = "Tomek";
+        int firstNumber = 3;
+        int secondNumber = 2;
+        int result = firstNumber + secondNumber;
+
+        System.out.println(result);
+        System.out.println(name);
+
+        name = "Robert";
+
+        System.out.println(name);
+
+    }
+}
