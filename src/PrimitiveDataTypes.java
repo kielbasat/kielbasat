254840347add060612ac39dd4b00c31e103af2b9
kielbasat <t.kielbasa@tlen.pl>
2022-01-30 10:38:14 +0100

    Added primitive data types

---
+++ src/PrimitiveDataTypes.java
@@ -0,0 +1,24 @@
+public class PrimitiveDataTypes {
+    public static void main(String[] args) {
+
+        byte firstNumber = 127; // 1 byte
+        short secondNumber = 32767; // 2 bytes
+        int thirdNumber = 2147483647; // 4 bytes
+        long fourthNumber = 9223372036854775807L; //8 bytes
+        float fifthNumber = 1.0000001f; // 4 bytes
+        double sixthNumber = 1.000000000000001d; // 8 bytes
+        boolean isTrue = true;
+        boolean isFalse = false;
+        char character = 'a';
+
+        System.out.println(firstNumber);
+        System.out.println(secondNumber);
+        System.out.println(thirdNumber);
+        System.out.println(fourthNumber);
+        System.out.println(fifthNumber);
+        System.out.println(sixthNumber);
+        System.out.println(isTrue);
+        System.out.println(isFalse);
+        System.out.println(character);
+    }
+}
