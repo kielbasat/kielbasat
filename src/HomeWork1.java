1460113fec42b4702dda9e3dcc5e5b309498158a
kielbasat <t.kielbasa@tlen.pl>
2022-03-03 11:23:43 +0100

    Reworked HM1

--- src/HomeWork1.java
+++ src/HomeWork1.java
@@ -3,24 +3,23 @@ import java.util.Scanner;
 public class HomeWork1 {
     public static void main(String[] args) {
 
-
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Please provide first number here: ");
-        float firstNumber = scanner.nextInt();
-        System.out.println("Please provide second number here: ");
-        float secondNumber = scanner.nextInt();
+        System.out.println("Please provide first number: ");
+        int firstNumber = scanner.nextInt();
+        System.out.println("Please provide second number: ");
+        int secondNumber = scanner.nextInt();
 
-        float addition = (firstNumber + secondNumber);
-        float subtraction = (firstNumber - secondNumber);
-        float division = (firstNumber / secondNumber);
-        float multiplication = (firstNumber * secondNumber);
-        float modulo = (firstNumber % secondNumber);
+        int additionResult = firstNumber + secondNumber;
+        int subtractionResult = firstNumber - secondNumber;
+        int multiplicationResult = firstNumber * secondNumber;
+        int divisionResult = firstNumber / secondNumber;
+        int moduloResult = firstNumber % secondNumber;
 
-        System.out.println("addition of Your numbers is: " + addition);
-        System.out.println("subtraction of Your numbers is: " + subtraction);
-        System.out.println("division of Your numbers is: " + division);
-        System.out.println("multiplication of Your numbers is: " + multiplication);
-        System.out.println("modulo of Your numbers is: " + modulo);
+        System.out.println(additionResult);
+        System.out.println(subtractionResult);
+        System.out.println(multiplicationResult);
+        System.out.println(divisionResult);
+        System.out.println(moduloResult);
 
 
     }
