35d89cfac3324caa42ffa1c4bbfb0fa218da865a
kielbasat <t.kielbasa@tlen.pl>
2022-03-04 10:27:16 +0100

    added for loop example

--- src/Switch.java
+++ src/Switch.java
@@ -1,20 +1,41 @@
+import java.util.Scanner;
+
 public class Switch {
 
     public static void main(String[] args) {
 
-        String danie = "Pizza";
+//        String danie = "Pizza";
+//
+//        switch (danie) {
+//            case "Pizza":
+//                System.out.println("Pizza was selected. Price is 22 zl");
+//                break;
+//            case "Salmon":
+//                System.out.println("Salmon was selected. Price is 42 zl");
+//            case "Chips":
+//                System.out.println("Chips were selected. Price is 10 zl");
+//            default:
+//                System.out.println("No such item");
+//        }
 
-        switch (danie) {
-            case "Pizza":
-                System.out.println("Pizza was selected. Price is 22 zl");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please provide number from 1 to 3 for chosen option \r\n" +
+                " 1 - Option 1 \n 2 - Option 2 \n 3 - Option 3");
+
+        int userInput = scanner.nextInt();
+
+        switch (userInput) {
+            case 1:
+                System.out.println("Option 1 selected");
                 break;
-            case "Salmon":
-                System.out.println("Salmon was selected. Price is 42 zl");
-            case "Chips":
-                System.out.println("Chips were selected. Price is 10 zl");
+            case 2:
+                System.out.println("Option 1 selected");
+                break;
+            case 3:
+                System.out.println("Option 1 selected");
+                break;
             default:
-                System.out.println("No such item");
+                System.out.println("Please select valid number. Range is from 1 to 3.");
         }
-
     }
 }
